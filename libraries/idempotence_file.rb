e5c9f79d9004ad13a59264fafe38b904620716dd
Caleb Severn <calsev@gmail.com>
2019-10-12 20:11:55 -0800

    Add library

---
+++ libraries/idempotence_file.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module IdempotenceFile
+  # This module implements shared utility code for consistency with dependent cookbooks
+  module Public
+
+  end
+  module Helper
+    TCB = 'idempotence_file'
+  end
+end
+
+Chef::Provider.include(IdempotenceFile::Helper)
+Chef::Recipe.include(IdempotenceFile::Helper)
+Chef::Resource.include(IdempotenceFile::Helper)
