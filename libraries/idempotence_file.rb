8c9e0d25ed3b4f07b1557965062f5264439bd91f
Caleb Severn <calsev@gmail.com>
2019-10-12 20:25:38 -0800

    Implement resource

--- libraries/idempotence_file.rb
+++ libraries/idempotence_file.rb
@@ -1,11 +1,44 @@
 # frozen_string_literal: true
 
 module IdempotenceFile
-  # This module implements shared utility code for consistency with dependent cookbooks
+  # This module implements public interfaces that are included in client code
   module Public
+    def idempotence_file?(name)
+      path = File.join(i_directory, name)
+      return File.exist?(path)
+    end
   end
+
+  # This module implements utility code
   module Helper
-    TCB = 'idempotence_file'
+    def i_directory
+      return '/var/chef/idempotence'
+    end
+
+    def create_directory
+      directory i_directory do
+        owner 'root'
+        group 'root'
+        mode 0o755
+        recursive true
+      end
+    end
+
+    def create_i_file(new_resource)
+      create_directory
+      file File.join(i_directory, new_resource.name) do
+        content 'Sentinel file'
+        owner 'root'
+        group 'root'
+        mode 0o644
+      end
+    end
+
+    def delete_i_file(new_resource)
+      file File.join(i_directory, new_resource.name) do
+        action :delete
+      end
+    end
   end
 end
 
