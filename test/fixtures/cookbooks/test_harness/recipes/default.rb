fa612cf8133c64078dcdc017f997647535195889
Caleb Severn <calsev@gmail.com>
2019-10-14 09:16:50 -0800

    Add recipe and test

--- test/fixtures/cookbooks/test_harness/recipes/default.rb
+++ test/fixtures/cookbooks/test_harness/recipes/default.rb
@@ -1,1 +1,48 @@
 # frozen_string_literal: true
+
+file '/tmp/before' do
+  owner 'root'
+  group 'root'
+  mode 0o644
+  not_if { idempotence_file?('the first') }
+end
+
+idempotence_file 'the first' do
+  action :delete
+end
+
+file '/tmp/empty' do
+  owner 'root'
+  group 'root'
+  mode 0o644
+  not_if { idempotence_file?('the first') }
+end
+
+idempotence_file 'the first'
+
+file '/tmp/after' do
+  owner 'root'
+  group 'root'
+  mode 0o644
+  not_if { idempotence_file?('the first') }
+end
+
+idempotence_file 'the first' do
+  action :delete
+end
+
+file '/tmp/deleted' do
+  owner 'root'
+  group 'root'
+  mode 0o644
+  not_if { idempotence_file?('the first') }
+end
+
+idempotence_file 'the first'
+
+file '/tmp/phoenix' do
+  owner 'root'
+  group 'root'
+  mode 0o644
+  not_if { idempotence_file?('the first') }
+end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,1 +1,71 @@
 # frozen_string_literal: true
+
+describe file '/tmp/before' do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+idempotence_file 'the first' do
+  action :delete
+end
+
+file '/tmp/empty' do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+idempotence_file 'the first'
+
+file '/tmp/after' do
+  it { should_not exist }
+end
+
+idempotence_file 'the first' do
+  action :delete
+end
+
+file '/tmp/deleted' do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+idempotence_file 'the first'
+
+file '/tmp/phoenix' do
+  it { should_not exist }
+end
+
+# Begin whitebox
+
+file '/var/chef' do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+file '/var/chef/idempotence' do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 0o755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+file '/var/chef/idempotence/the_first' do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 0o644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
